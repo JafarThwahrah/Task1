9b87613f3376555e88b3bf2c7700238331a1a12b
Jafar Thwahrah <jaffardawahreh2@gmail.com>
2023-03-14 22:21:38 +0300

    Modify Logout

--- src/customHooks/Logout.jsx
+++ src/customHooks/Logout.jsx
@@ -1,11 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { MyContext } from "../App";
-
+import getCookie from "./GetCookie";
+import axios from "axios";
 const useLogout = () => {
   const myContextValue = useContext(MyContext);
   const navigate = useNavigate();
   const logout = () => {
+    const token = getCookie("token");
+    const headers = { Authorization: `${token}` };
+    axios
+      .get("https://staging-blockchain-payment.livaat.com/api/admins/logout", {
+        headers,
+      })
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+      });
     document.cookie = "id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "name=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
